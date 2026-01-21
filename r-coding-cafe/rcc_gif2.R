7727cc8d698af5f7facf553fe0954b56ffb1b291
Zheer Kejlberg Al-Mashhadi <102064238+zheer-kejlberg@users.noreply.github.com>
2026-01-21 23:01:11 +0100

    fixes

--- r-coding-cafe/rcc_gif2.R
+++
@@ -1,7 +0,0 @@
-message('A collection of useful bits from each lesson')
-lessons = matrix(
-  c("Lesson 1","Minimal Reproducible Examples", 
-    "Lesson 2","Debugging",
-    "Lesson 3","Data types",
-    "Lesson 4","..."), ncol=4, nrow=2)
-apply(lessons, 2, paste0, collapse=": ")
--- r-coding-cafe/rcc_gif3.r
+++
@@ -1,1 +0,0 @@
-message('Some practical info')
